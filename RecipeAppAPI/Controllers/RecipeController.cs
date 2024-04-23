51fa169b430f412855663405ffef0e98b0538513
Muhammed Haddad <diffractingwave@gmail.com>
2024-04-23 17:28:23 +0100

    DB context field added + Connected to database + Get Request

---
+++ RecipeAppAPI/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeAppAPI.Models;
+using System.Linq;
+
+namespace RecipeAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public RecipeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Recipe
+        [HttpGet]
+        public IActionResult GetAllRecipes()
+        {
+            var recipes = _dbContext.Recipes.ToList();
+            return Ok(recipes);
+        }
+    }
+}
---
+++ RecipeAppAPI/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeAppAPI.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<RecipeTable> Recipes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
